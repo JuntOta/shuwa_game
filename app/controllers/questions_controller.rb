eec9f92939249932fb2723023958dca0af808021
JuntOta <junto.ota@ezweb.ne.jp>
2021-05-19 20:08:16 +0900

    createアクションのパラメーター作成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,9 +4,17 @@ class QuestionsController < ApplicationController
   end
 
   def new
+    @question = Question.new
   end
 
   def create
+    Question.create(question_params)
   end
   
+  private
+  
+  def question_params
+    params.require(:question).permit(:video, :correct, :wrong_1, :wrong_2, :wrong_3, :hint)
+  end
+
 end
